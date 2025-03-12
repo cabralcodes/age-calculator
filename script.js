63457c550217c0171cea35d8dcab0a5da3b39444
Matheus cabral codes <matheusaugustodev.web@gmail.com>
2025-03-12 14:48:49 -0300

    terminei os estilos, só falta a parte do js, to com real dúvida

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ const anoAtual = data.getFullYear();
 const erro = document.querySelector(".error");
 const but = document.querySelector(".botao");
 
+const DayResul = document.querySelector(".roxo")
+
+
+
+
+
+
 const dia = document.getElementById("numday");
 const mes = document.getElementById("nummonth");
 const ano = document.getElementById("numyear");
 
 but.addEventListener("click",()=> {
-    if (dia > 31) {
-        console.log(erro)
-    }
+  
 })
 
