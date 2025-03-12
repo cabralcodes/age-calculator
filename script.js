a3c27bdd5db902da49f19ba47dc43e616a501c56
Matheus cabral codes <matheusaugustodev.web@gmail.com>
2025-03-12 11:46:45 -0300

    ja avancei boa parte só falta mexer no traamento de erros e o calculo

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const data = new Date;
 
 const diaAtual = data.getDay();
-const mesAtual = data.getMonth() +1;
+const mesAtual = data.getMonth();
 const anoAtual = data.getFullYear();
+
+const erro = document.querySelector(".error");
+const but = document.querySelector(".botao");
 
 const dia = document.getElementById("numday");
 const mes = document.getElementById("nummonth");
 const ano = document.getElementById("numyear");
 
-function calcular() {
-    const diaResul = diaAtual - dia
-    const mesResul = mesAtual - mes
-    const anoResul = anoAtual - ano
-    console.log(diaResul)
-    console.log(mesResul)
-    console.log(anoResul)
-}
+but.addEventListener("click",()=> {
+    if (dia > 31) {
+        console.log(erro)
+    }
+})
 
